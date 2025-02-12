00d51d4f62dbdc2198d8e6577ff3c49339eeb224
dog-wang-wang <dog-wang-wang@1123019893@qq.com>
2025-02-12 16:42:39 +0800

    新增了一个引导页

---
+++ app/src/main/java/com/dorameet/myapplication/IndexActivity.kt
@@ -0,0 +1,30 @@
+package com.dorameet.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+
+class IndexActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_index)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        findViewById<MaterialButton>(R.id.btn_first)
+            .setOnClickListener {
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+        findViewById<MaterialButton>(R.id.btn_second)
+            .setOnClickListener {
+                startActivity(Intent(this, SecondActivity::class.java))
+            }
+    }
+}
